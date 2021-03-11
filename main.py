a7d3fe29c9113470000646ee54f9d385d881f779
martinkarlik <mkarli18@student.aau.dk>
2021-03-11 09:50:38 +0100

    Created develop branch.

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print("Carnivores eat herbivores. Miniproject done.")
+    print("Carnivores eat herbivores.")
+    print("Miniproject done.")
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
