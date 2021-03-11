280c6257a5405e2d5fbd1af8251134e568763876
martinkarlik <mkarli18@student.aau.dk>
2021-03-11 09:49:35 +0100

    Initial commit.

---
+++ main.py
@@ -0,0 +1,8 @@
+# This is a sample Python script.
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("Carnivores eat herbivores. Miniproject done.")
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
