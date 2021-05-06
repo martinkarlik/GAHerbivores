5af14bcfbad617f7607058579acbade005bfd82a
Victor Buch <47211196+VictorBuch@users.noreply.github.com>
2021-05-06 15:03:25 +0200

    Sigmoid

--- neural_network.py
+++ neural_network.py
@@ -7,14 +7,13 @@ def initiate_random_weights():
 
 
 def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
     """
     Write the sigmoid function.
     """
-    return x
 
 
 def forward_propagate(weights, features):
-
     """
     Multiply the features (plant's information) with the weights and then sigmoid them.
     """
